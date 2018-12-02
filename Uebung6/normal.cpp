7e20fe2c70b532ed4f8cf06f99f539e413e10f0d
Amandus <amandusbutzer@gmx.de>
2018-12-02 20:22:38 +0100

    Add U6 files

---
+++ Uebung6/normal.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Amandus Butzer on 12/2/2018.
+//
+#include <io.hh>
---
+++ Uebung6/readvector.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Amandus Butzer on 12/2/2018.
+//
+#include <io.hh>
+#include <vector>
+
+using namespace std
+
+int main () {
+    vector<double>  read = read_vector();
+    cout << read << endl;
+    return 0;
+}
